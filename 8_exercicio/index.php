66ddef1299a18aba8d885cc5ce2d6043c524155e
Thauan Nery <tpcn2000@gmail.com>
2024-06-13 15:32:24 -0300

    excercios de escopo

---
+++ 8_exercicio/index.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = "Thauan"; //string
+    $idade = 23; //int
+    $info = ['altura' => 1.80, 'peso' => 80, 'sexo' => 'masculino' ]; //array
+
+    echo "$nome tem $idade de idade, abaixo segue com outros dados pessoas <br>";
+
+    print_r ($info);
+?>
---
+++ 9_exercicio/index.php
@@ -0,0 +1,10 @@
+<?php
+    $primeiro = 59;
+    $segundo = 87;
+
+    echo $primeiro;
+    echo "+";
+    echo $segundo;
+    echo "=";
+    echo $primeiro + $segundo;
+?>
