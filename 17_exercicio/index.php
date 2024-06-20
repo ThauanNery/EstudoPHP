04cfe9ca5bfc16a676970e2e9d2c23b7670edfdb
Thauan Nery <tpcn2000@gmail.com>
2024-06-20 16:05:44 -0300

    operadores logicos e operadores < > >= <=

---
+++ 17_exercicio/index.php
@@ -0,0 +1,16 @@
+<?php
+    if(15 > 5 && "João" === "João")
+    {
+        echo "Ambos os resultados são true 1 <br>";
+    }
+
+    if("teste" > 5 && 1)
+    {
+        echo "Ambos os resultados são true 2 <br>";
+    }
+
+    if(2 == 3 && 5 >= 3)
+    {
+        echo "Ambos os resultados são true 3 <br>";
+    }
+?>
---
+++ 18_exercicio/index.php
@@ -0,0 +1,16 @@
+<?php
+    if(12 < 5 || "João" === "João")
+    {
+        echo "Um ou ambos os valores são verdadeiros 1 <br>";
+    }
+
+    if(1 > 5 || 1)
+    {
+        echo "Um ou ambos os valores são verdadeiros 2 <br>";
+    }
+
+    if(20 === "20" && 51 >= 31)
+    {
+        echo "Um ou ambos os valores são verdadeiros 2 <br>";
+    }
+?>
